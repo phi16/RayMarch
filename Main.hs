62c94ba57fe2eb6266b399af6debec04dc8c027c
phi16 <phi16.256@gmail.com>
2014-06-22 12:15:37 +0900

    Fixed heidrichSeidel

--- Main.hs
+++ Main.hs
@@ -33,7 +33,7 @@ main = runMarcher config world where
   }
   light = Vector (0,-5,10)
   bxSz = Vector (0.2,0.2,0.2)
-  metalic c = (alpha 0.5 mirror`lighten`(rainbow 0.1 light $ halfLambert light)`lighten`heidrichSeidel 1.0 (Vector (0,0,1)) light)`darken`(alpha 0.5 $ ambientOcclusion 3.0 0.1`lighten`softShadow 8.0 light)
+  metalic c = (alpha 0.5 mirror`lighten`(rainbow 0 light $ halfLambert light)`lighten`heidrichSeidel 5.0 5.0 rel light)`darken`(alpha 0.5 $ ambientOcclusion 3.0 0.1`lighten`softShadow 8.0 light)
   ground = plane (Vector (0,0,1)) 0 $ metalic $ Color (0.8,0.4,0.0)
   bxs = repeatX 2 $ repeatY 2 $ transpose (Vector (0,0,0.2)) $ box (Vector (0.2,0.2,0.2)) $ metalic white
   bound = invert $ sphere 100 $ emission black
@@ -43,3 +43,7 @@ main = runMarcher config world where
   spe = transpose (Vector (0,0,0.8)) $ sphere 0.2 $ metalic white
   obj = transpose (Vector (0,0,0.3)) $ rotate (xRotate (rad 10)`prod`yRotate (rad (-20))) $ spr <|> pal <|> spe
   dists = bound <|> gnd <|> obj
+  rel = (xRotate (rad 10)`prod`yRotate (rad (-20)))`apply`Vector (0,1,0)
+
+
+
